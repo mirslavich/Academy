b4c35d155d6f81eba49bbbb1e7bb2bf6402f67c5
MireX <mirslavich@gmail.com>
2022-07-04 01:51:29 +0300

    Download not working

--- AcademyHomework3.TaxiStation/TaxiStation.cs
+++ AcademyHomework3.TaxiStation/TaxiStation.cs
@@ -12,20 +12,19 @@ namespace AcademyHomework3.TaxiStation
         }
         public void LoadFromFile()
         {
-            //var filePath = @"TaxiStation.txt";
-            //var taxiStringRepresentation= File.ReadAllText(filePath);
-            // _collection= new List<Taxi>();
-            
-            //foreach (var item in taxiStringRepresentation)
-            //{
-            //    var data = item.Split('-');
-            //    var taxi = new Taxi(int.Parse(data[0]), int.Parse(data[1]), int.Parse(data[2]),int.Parse(data[3]));
-            //    _collection.Add(taxi);
-            //}
-            //foreach (var item in _collection)
-            //{
-            //    Console.WriteLine($"Cat {item.Id} - color-{item.Speed}");
-            //}
+            var filePath = @"TaxiStation.txt";
+            var taxiStringRepresentation= File.ReadAllText(filePath);
+            _collection= new List<Taxi>();
+            foreach (string item in taxiStringRepresentation)
+            {
+                var data = item.Split('-');
+                var taxi = new Taxi(int.Parse(data[0]), int.Parse(data[1]), int.Parse(data[2]),int.Parse(data[3]));
+                _collection.Add(taxi);
+            }
+            foreach (var item in _collection)
+            {
+                Console.WriteLine($"Id: {item.Id} - Consumption: {item.Consumption}- Cost: {item.Cost}- Speed: {item.Speed}");
+            }
         }
         public List<Taxi> GetTaxiBySpeed(int speedMin, int speedMax)
         { 
