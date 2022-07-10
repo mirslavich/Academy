77230ee16cc8bbffcf59e99c5476483937e439e4
MireX <mirslavich@gmail.com>
2022-07-11 00:34:39 +0300

    Alfa 0.5

---
+++ StringParsing/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace StringParsing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //var filePathRead = @"sample.txt";
+            var fileTestPathR = @"sample1.txt";
+            //var fileTestPathR = @"test.txt";
+            var fileTestPathW = @"testW.txt";
+
+            var regexWord = new Regex(@"(?<ApWord>((\w+)(')(\w+)))|(?<HyWord>((\w+)(-)(\w+)))|(?<Rest>\w+)"); //only woord not always correct
+            var regexSentence = new Regex(@"((?:[A-Z]\.|[^\.!?])+)[\.!?]"); // only sentence not always correct
+            var regexPunctuationMarks = new Regex(@"");   // need to add punction regex
+
+            var fr = new TextFile();
+            //var fOrder = new FileInAlphabeticalOrder();
+            var matches2 = regexWord.Matches(fr.GetFullText(fileTestPathR))
+                .GroupBy(x => x.Value)
+                .Where(r => r.Count() >= 1)
+                .OrderBy(m => m.Key);   
+            foreach (var item in matches2)
+            {
+                fr.SaveWordsInFile(fileTestPathW,item.Key,item.Count());
+                //Console.WriteLine("{0}: \t{1}", item.Key, item.Count());
+            }
+
+            var matches3 = regexSentence.Matches(fr.GetFullText(fileTestPathR)).ToList();
+            ////.Select(m => m.Value)
+            //.GroupBy(
+            //x => x.Value,
+            //y => y.Value.Length,
+            //(pString, pString2) => new
+            //{
+            //    Sentence = pString,
+            //    SentenceLenght = pString2
+            //});
+            int maxLength=0;
+            string maxSentense="";
+            foreach (var item in matches3)
+            {
+                
+                if (item.Value.Length>maxLength)
+                { 
+                    maxLength= item.Value.Length;
+                    maxSentense=item.Value;
+                }
+            }
+
+            Console.WriteLine(maxSentense);
+           
+        }
+        
+    }
+}
---
+++ StringParsing/TextFile.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace StringParsing
+{
+    public class TextFile
+    {
+        
+        public string GetFullText(string filePath)
+        {
+            using (var sr= new StreamReader (filePath,Encoding.UTF8))
+            {
+                var allText = sr.ReadToEnd();
+                return allText;
+            }
+
+        }
+
+        public void SaveWordsInFile(string filePathWrite, string word, int count)
+        {
+            using (var sw = new StreamWriter(filePathWrite, true, Encoding.UTF8))
+            {
+                sw.WriteLine($"Word: {word} \t {count} times");
+            }
+        }
+
+        public void SaveSmthInFile(string filePathWrite, string theLongestSentence, string theShortestSentence, char letter)
+        {
+            using (var sw = new StreamWriter(filePathWrite, true, Encoding.UTF8))
+            {
+                sw.Write($">>>>>>>The longest sentene: \n {theLongestSentence}");
+                sw.Write($">>>>>>>The shortest sentene: \n {theShortestSentence}");
+                sw.Write($">>>>>>>>The most common letter: {letter}");
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
