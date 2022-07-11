24775334b3b6787ce2b1230d1fd46e4d095ef597
MireX <mirslavich@gmail.com>
2022-07-11 04:15:02 +0300

    Alfa 0.6

--- StringParsing/Program.cs
+++ StringParsing/Program.cs
@@ -8,51 +8,96 @@ namespace StringParsing
         static void Main(string[] args)
         {
             //var filePathRead = @"sample.txt";
-            var fileTestPathR = @"sample1.txt";
-            //var fileTestPathR = @"test.txt";
-            var fileTestPathW = @"testW.txt";
+            //var fileTestPathR = @"sample1.txt";
+            var fileTestPathR = @"testR.txt";
+            var wordsTestPathW = @"testWordsW.txt";
+            var sentenceTestPathW = @"testSentenceW.txt";
 
-            var regexWord = new Regex(@"(?<ApWord>((\w+)(')(\w+)))|(?<HyWord>((\w+)(-)(\w+)))|(?<Rest>\w+)"); //only woord not always correct
-            var regexSentence = new Regex(@"((?:[A-Z]\.|[^\.!?])+)[\.!?]"); // only sentence not always correct
+
+            
+
             var regexPunctuationMarks = new Regex(@"");   // need to add punction regex
 
-            var fr = new TextFile();
-            //var fOrder = new FileInAlphabeticalOrder();
-            var matches2 = regexWord.Matches(fr.GetFullText(fileTestPathR))
-                .GroupBy(x => x.Value)
-                .Where(r => r.Count() >= 1)
-                .OrderBy(m => m.Key);   
-            foreach (var item in matches2)
-            {
-                fr.SaveWordsInFile(fileTestPathW,item.Key,item.Count());
-                //Console.WriteLine("{0}: \t{1}", item.Key, item.Count());
-            }
+            var workWithFile = new TextFile(fileTestPathR, wordsTestPathW, sentenceTestPathW);
+            GetWordsAndTheirNumbersByLinq(workWithFile);
+            var getLetter = GetLetterWithMaxRepetititonByLinq(workWithFile);
+            var getMaxSentence = GetSentenceWithMaxLetters(workWithFile);
 
-            var matches3 = regexSentence.Matches(fr.GetFullText(fileTestPathR)).ToList();
-            ////.Select(m => m.Value)
-            //.GroupBy(
-            //x => x.Value,
-            //y => y.Value.Length,
-            //(pString, pString2) => new
-            //{
-            //    Sentence = pString,
-            //    SentenceLenght = pString2
-            //});
-            int maxLength=0;
-            string maxSentense="";
-            foreach (var item in matches3)
+            
+
+
+
+
+
+
+            //Console.WriteLine($"Symbol MAX: {letterWithMaxValue} value is: {letterMaxValue}");
+            /*
+            var letterMaxValue = 0;
+            char? letterWithMaxValue = null;
+            string st = fr.GetFullText(fileTestPathR).ToLower();
+            var counts = st.Where(c => char.IsLetter(c))
+                            .GroupBy(c => c)
+                            .ToDictionary(grp => grp.Key, grp => grp.Count());
+            foreach (var item in counts)
             {
-                
-                if (item.Value.Length>maxLength)
-                { 
-                    maxLength= item.Value.Length;
-                    maxSentense=item.Value;
+                Console.WriteLine($"Symbol: {item.Key} value is: {item.Value}");
+
+
+                if (item.Value > letterMaxValue)
+                {
+                    letterMaxValue = item.Value;
+                    letterWithMaxValue = item.Key;
                 }
             }
+            Console.WriteLine($"Symbol MAX: {letterWithMaxValue} value is: {letterMaxValue}");
+            */
+        }
+        static void GetWordsAndTheirNumbersByLinq(TextFile workWithFile)
+        {
+            var regexWord = new Regex(@"(?<ApWord>((\w+)(')(\w+)))|(?<HyWord>((\w+)(-)(\w+)))|(?<Rest>\w+)"); //only woord not always correct
+            var matchesWords = regexWord.Matches(workWithFile.GetFullText())
+                .GroupBy(x => x.Value)
+                .Where(r => r.Count() >= 1)
+                .OrderBy(m => m.Key);
+            foreach (var item in matchesWords)
+            {
+                workWithFile.SaveWordsInFile(item.Key, item.Count());
+            }
+        }
+        /*
+        static string GetSentenceWithMaxLetters(TextFile workWithFile)
+        {
+            var regexSentence = new Regex(@"((?:[A-Z]\.|[^\.!?])+)[\.!?]"); // only sentence not always correct
+            var matchesMaxSentence = regexSentence.Matches(workWithFile.GetFullText().ToList());
+            
+            int maxLength = 0;
+            string maxSentense = "";
+            foreach (var item in matchesMaxSentence)
+            {
+                if (item.Value.Length > maxLength)
+                {
+                    maxLength = item.Value.Length;
+                    maxSentense = item.Value;
+                }
+            }
+            return maxSentense;
+            
+        }
+        */
+        static string GetLetterWithMaxRepetititonByLinq(TextFile worsWithFile)
+        {
+            string st = worsWithFile.GetFullText().ToLower();
+            var count = st.Where(c => char.IsLetter(c))
+                            .GroupBy(litteral => litteral)
+                            .Select(item => new { Key = item.Key, Count = item.Count() })
+                            .MaxBy(item => item.Count);
 
-            Console.WriteLine(maxSentense);
-           
+            return count.Key + " " + count.Count;
         }
         
     }
 }
+        
+
+
+    
--- StringParsing/TextFile.cs
+++ StringParsing/TextFile.cs
@@ -4,10 +4,14 @@ namespace StringParsing
 {
     public class TextFile
     {
-        
-        public string GetFullText(string filePath)
+        private string _pathForReading;
+        private string _pathForWritingWords;
+        private string _pathforWritingSentence;
+        public TextFile
+        }
+        public string GetFullText()
         {
-            using (var sr= new StreamReader (filePath,Encoding.UTF8))
+            using (var sr= new StreamReader (_filePathForReading,Encoding.UTF8))
             {
                 var allText = sr.ReadToEnd();
                 return allText;
@@ -15,7 +19,7 @@ namespace StringParsing
 
         }
 
-        public void SaveWordsInFile(string filePathWrite, string word, int count)
+        public static void SaveWordsInFile(string filePathWrite, string word, int count)
         {
             using (var sw = new StreamWriter(filePathWrite, true, Encoding.UTF8))
             {
@@ -23,13 +27,13 @@ namespace StringParsing
             }
         }
 
-        public void SaveSmthInFile(string filePathWrite, string theLongestSentence, string theShortestSentence, char letter)
+        public void SaveSmthInFile(string filePathWrite, string theLongestSentence, string theShortestSentence, char letter, int letterRepetition)
         {
             using (var sw = new StreamWriter(filePathWrite, true, Encoding.UTF8))
             {
                 sw.Write($">>>>>>>The longest sentene: \n {theLongestSentence}");
                 sw.Write($">>>>>>>The shortest sentene: \n {theShortestSentence}");
-                sw.Write($">>>>>>>>The most common letter: {letter}");
+                sw.Write($">>>>>>>>The most common letter: {letter} \t {letterRepetition} times");
             }
         }
 
