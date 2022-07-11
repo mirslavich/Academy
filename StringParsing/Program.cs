11f1911274789c186d4a25a2f857d2ec9c86c05c
MireX <mirslavich@gmail.com>
2022-07-11 18:45:38 +0300

    beta

--- StringParsing/Program.cs
+++ StringParsing/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 namespace StringParsing
 {
@@ -8,54 +10,27 @@ namespace StringParsing
         static void Main(string[] args)
         {
             //var filePathRead = @"sample.txt";
-            //var fileTestPathR = @"sample1.txt";
-            var fileTestPathR = @"testR.txt";
+            var fileTestPathR = @"sample1.txt";
+            //var fileTestPathR = @"testR.txt";
             var wordsTestPathW = @"testWordsW.txt";
             var sentenceTestPathW = @"testSentenceW.txt";
 
 
-            
-
+            var regexWord = new Regex(@"(?<ApWord>((\w+)(')(\w+)))|(?<HyWord>((\w+)(-)(\w+)))|(?<Rest>\w+)"); //only woord not always correct
+            var regexSentence = new Regex(@"((?:[A-Z]\.|[^\.!?])+)[\.!?]"); // only sentence not always correct
             var regexPunctuationMarks = new Regex(@"");   // need to add punction regex
 
             var workWithFile = new TextFile(fileTestPathR, wordsTestPathW, sentenceTestPathW);
-            GetWordsAndTheirNumbersByLinq(workWithFile);
-            var getLetter = GetLetterWithMaxRepetititonByLinq(workWithFile);
-            var getMaxSentence = GetSentenceWithMaxLetters(workWithFile);
-
+            GetWordsAndTheirNumbersByLinq(workWithFile, regexWord);
+            var isMaxRepetitionLetter = GetLetterWithMaxRepetititonByLinq(workWithFile);
+            var isTheLongestSentence = GetSentenceWithMaxLetters(workWithFile, regexSentence);
+            var isTheShortestSentence = GetTheShortestSentence(workWithFile, regexSentence, regexWord);
+            workWithFile.SaveSmthInFile(isTheLongestSentence, isTheShortestSentence, isMaxRepetitionLetter);
             
-
-
-
-
-
-
-            //Console.WriteLine($"Symbol MAX: {letterWithMaxValue} value is: {letterMaxValue}");
-            /*
-            var letterMaxValue = 0;
-            char? letterWithMaxValue = null;
-            string st = fr.GetFullText(fileTestPathR).ToLower();
-            var counts = st.Where(c => char.IsLetter(c))
-                            .GroupBy(c => c)
-                            .ToDictionary(grp => grp.Key, grp => grp.Count());
-            foreach (var item in counts)
-            {
-                Console.WriteLine($"Symbol: {item.Key} value is: {item.Value}");
-
-
-                if (item.Value > letterMaxValue)
-                {
-                    letterMaxValue = item.Value;
-                    letterWithMaxValue = item.Key;
-                }
-            }
-            Console.WriteLine($"Symbol MAX: {letterWithMaxValue} value is: {letterMaxValue}");
-            */
         }
-        static void GetWordsAndTheirNumbersByLinq(TextFile workWithFile)
+        static void GetWordsAndTheirNumbersByLinq(TextFile workWithFile,Regex regexWord)
         {
-            var regexWord = new Regex(@"(?<ApWord>((\w+)(')(\w+)))|(?<HyWord>((\w+)(-)(\w+)))|(?<Rest>\w+)"); //only woord not always correct
-            var matchesWords = regexWord.Matches(workWithFile.GetFullText())
+             var matchesWords = regexWord.Matches(workWithFile.GetFullText())
                 .GroupBy(x => x.Value)
                 .Where(r => r.Count() >= 1)
                 .OrderBy(m => m.Key);
@@ -64,12 +39,10 @@ namespace StringParsing
                 workWithFile.SaveWordsInFile(item.Key, item.Count());
             }
         }
-        /*
-        static string GetSentenceWithMaxLetters(TextFile workWithFile)
+        
+        static string GetSentenceWithMaxLetters(TextFile workWithFile, Regex regexSentence)
         {
-            var regexSentence = new Regex(@"((?:[A-Z]\.|[^\.!?])+)[\.!?]"); // only sentence not always correct
-            var matchesMaxSentence = regexSentence.Matches(workWithFile.GetFullText().ToList());
-            
+            var matchesMaxSentence = regexSentence.Matches(workWithFile.GetFullText()).ToList();
             int maxLength = 0;
             string maxSentense = "";
             foreach (var item in matchesMaxSentence)
@@ -81,20 +54,34 @@ namespace StringParsing
                 }
             }
             return maxSentense;
-            
         }
-        */
+
+        static string GetTheShortestSentence(TextFile workWithFile, Regex regexSentence, Regex regexWord)
+        {
+            var minTimesWordsInSentence = int.MaxValue;
+            string shortSentence = "";
+            var matchesShortSentence = regexSentence.Matches(workWithFile.GetFullText()).ToList();
+            foreach (var item in matchesShortSentence)
+            {
+                var coutWordsInSentence = 0;
+                var matchesWordsInSentence = regexWord.Matches(item.Value);
+                if (matchesWordsInSentence.Count < minTimesWordsInSentence)
+                {
+                    shortSentence = item.Value;
+                }
+            }
+            return shortSentence;
+        }
+        
         static string GetLetterWithMaxRepetititonByLinq(TextFile worsWithFile)
         {
             string st = worsWithFile.GetFullText().ToLower();
             var count = st.Where(c => char.IsLetter(c))
-                            .GroupBy(litteral => litteral)
-                            .Select(item => new { Key = item.Key, Count = item.Count() })
-                            .MaxBy(item => item.Count);
-
+                .GroupBy(litteral => litteral)
+                .Select(item => new { Key = item.Key, Count = item.Count() })
+                .MaxBy(item => item.Count);
             return count.Key + " " + count.Count;
         }
-        
     }
 }
         
--- StringParsing/TextFile.cs
+++ StringParsing/TextFile.cs
@@ -7,42 +7,39 @@ namespace StringParsing
         private string _pathForReading;
         private string _pathForWritingWords;
         private string _pathforWritingSentence;
-        public TextFile
+
+        public TextFile(string pathForReading,string pathForWritingWords, string pathforWritingSentence)
+        {
+            _pathForReading = pathForReading;
+            _pathForWritingWords = pathForWritingWords; 
+            _pathforWritingSentence=pathforWritingSentence;
         }
+        
         public string GetFullText()
         {
-            using (var sr= new StreamReader (_filePathForReading,Encoding.UTF8))
+            using (var sr= new StreamReader (_pathForReading, Encoding.UTF8))
             {
                 var allText = sr.ReadToEnd();
                 return allText;
             }
-
         }
 
-        public static void SaveWordsInFile(string filePathWrite, string word, int count)
+        public  void SaveWordsInFile(string word, int count)
         {
-            using (var sw = new StreamWriter(filePathWrite, true, Encoding.UTF8))
+            using (var sw = new StreamWriter(_pathForWritingWords, true, Encoding.UTF8))
             {
                 sw.WriteLine($"Word: {word} \t {count} times");
             }
         }
 
-        public void SaveSmthInFile(string filePathWrite, string theLongestSentence, string theShortestSentence, char letter, int letterRepetition)
+        public void SaveSmthInFile(string theLongestSentence, string theShortestSentence, string letterAndLetterRepetition)
         {
-            using (var sw = new StreamWriter(filePathWrite, true, Encoding.UTF8))
+            using (var sw = new StreamWriter(_pathforWritingSentence, true, Encoding.UTF8))
             {
-                sw.Write($">>>>>>>The longest sentene: \n {theLongestSentence}");
-                sw.Write($">>>>>>>The shortest sentene: \n {theShortestSentence}");
-                sw.Write($">>>>>>>>The most common letter: {letter} \t {letterRepetition} times");
+                sw.Write($"\t>>>>>>>The longest sentene: \n {theLongestSentence}");
+                sw.Write($"\n\t>>>>>>>Firs the shortest sentence: \n {theShortestSentence}");
+                sw.Write($"\n\t>>>>>>>>The most common letter:\n {letterAndLetterRepetition}");
             }
         }
-
-
-
-
-
-
-
-
     }
 }
